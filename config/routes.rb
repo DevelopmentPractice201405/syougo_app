919a0ce26c85a8651e093c5a804baadee1f7e1e9
ooshirosyougo <syougo_o12b@stu.kbc.ac.jp>
2014-06-27 13:02:54 +0900

    Make a basic User model (including secure passwords)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 SampleApp::Application.routes.draw do
-
   get "users/new"
 
   root to: 'static_pages#home'
@@ -8,8 +7,7 @@ SampleApp::Application.routes.draw do
 
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
-  match '/contact', to: 'static_pages#contact'  
-
+  match '/contact', to: 'static_pages#contact'
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
