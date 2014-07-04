2e0916bb3940b6c4cca5a942916c473d226ebf11
ooshirosyougo <syougo_o12b@stu.kbc.ac.jp>
2014-07-04 12:27:25 +0900

    Add SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,6 +8,11 @@ SampleApp::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # アプリケーションへのすべてのアクセスをSSL経由にし、
+  # Strict-Transport-Securityを使用し、
+  # secure cookiesを使用する。
+  config.force_ssl = true
+
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
